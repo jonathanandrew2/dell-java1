8f686f5c2f3918fe6a03bbf34c4d29f002fe9985
Jonathan <jonathan_hernandez2@dell.com>
2019-02-01 16:31:42 -0600

    my hw asssignments

---
+++ Vehicle/src/Vehicle.java
@@ -0,0 +1,17 @@
+
+public abstract class Vehicle {
+	
+	private String licenseNumber;
+	private String make;
+	private String model;
+	private int price;
+	
+	
+	public String getLicenseNumber() {
+		return licenseNumber;
+		
+	}
+	
+	
+ public abstract void printDescription();
+}
--- driverslicense/src/driverslicense/LicenseProgram.java
+++ driverslicense/src/driverslicense/LicenseProgram.java
@@ -1,4 +1,4 @@
-package driverslicense;
+                 package driverslicense;
 
 import java.time.LocalDate;
 import java.time.Period;
@@ -9,34 +9,23 @@ public class LicenseProgram {
 		
 		
 	DriversLicense dL = new DriversLicense();
-	dL.firstName = "Jon";
+	dL.dobDD = 22;
+	dL.dobMM = 9;
+	dL.dobYYYY = 1992;
+	dL.firstName = "Jonathan";
 	dL.lastName = "Hernandez";
-	dL.dob = 1992;
-	int year = 0;
-	int month = 0;
-	int day = 0;
+			
+
 	
 	
 	System.out.println("Name: " + dL.getFirstName() + " " + dL.getLastName());
-	System.out.println("Age : " + (dL.getDob()));
+	System.out.println("Age : " + dL.ageOfPerson());
 	
 		System.out.println();
 
 	}
 	
-	public static void ageOfPerson() {
-		
-		LocalDate today = LocalDate.now(); // today's date
-		LocalDate birthday = LocalDate.of(1992, 01, 03); // date of birth
-
-		Period period = Period.between(birthday, today);
-
-		int age = period.getYears(); // calculate the period between DOB and today in years
-		System.out.println(age);
-
-		
-		
-	}
+	
 
 
 }
